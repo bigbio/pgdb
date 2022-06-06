3f2a411695a62d502b25173ae581dd84e2a4f5bc
Yasset Perez-Riverol <ypriverol@gmail.com>
2022-06-06 11:36:59 +0100

    merge template 2.4.0

--- lib/WorkflowPgdb.groovy
+++ lib/WorkflowPgdb.groovy
@@ -8,12 +8,12 @@ class WorkflowPgdb {
     // Check and validate parameters
     //
     public static void initialise(params, log) {
-        genomeExistsError(params, log)
-
-        if (!params.fasta) {
-            log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
-            System.exit(1)
-        }
+//         genomeExistsError(params, log)
+// 
+//         if (!params.fasta) {
+//             log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
+//             System.exit(1)
+//         }
     }
 
     //
