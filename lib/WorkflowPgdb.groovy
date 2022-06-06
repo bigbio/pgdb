8e02c1f94e99aa6d30637f6123ddb03b7057e8e4
Yasset Perez-Riverol <ypriverol@gmail.com>
2022-06-06 11:33:42 +0100

    merge template 2.4.0

--- lib/WorkflowPgdb.groovy
+++ lib/WorkflowPgdb.groovy
@@ -46,14 +46,14 @@ class WorkflowPgdb {
     //
     // Exit pipeline if incorrect --genome key provided
     //
-    private static void genomeExistsError(params, log) {
-        if (params.genomes && params.genome && !params.genomes.containsKey(params.genome)) {
-            log.error "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
-                "  Genome '${params.genome}' not found in any config files provided to the pipeline.\n" +
-                "  Currently, the available genome keys are:\n" +
-                "  ${params.genomes.keySet().join(", ")}\n" +
-                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-            System.exit(1)
-        }
-    }
+//     private static void genomeExistsError(params, log) {
+//         if (params.genomes && params.genome && !params.genomes.containsKey(params.genome)) {
+//             log.error "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+//                 "  Genome '${params.genome}' not found in any config files provided to the pipeline.\n" +
+//                 "  Currently, the available genome keys are:\n" +
+//                 "  ${params.genomes.keySet().join(", ")}\n" +
+//                 "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+//             System.exit(1)
+//         }
+//     }
 }
